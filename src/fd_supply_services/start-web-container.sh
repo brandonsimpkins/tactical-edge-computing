1052180a4d3524e775b741b9bf3c69ebaaa658bb
Brandon Simpkins <brandonsimpkins@gmail.com>
2018-07-05 21:50:35 +0000

    Added the docker-compose entrypoint script
    Meant to add this into the previous checkin. This script automatically
    migrates the database schema and starts the django server on container
    startup.

---
+++ src/fd_supply_services/start-web-container.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# require that commands run successfully
+set -e
+
+# wait for Postgres to start
+# sleep 10
+
+# migrate the database schema as necessary
+python manage.py migrate
+
+# run the rest services
+python manage.py runserver 0.0.0.0:8000
