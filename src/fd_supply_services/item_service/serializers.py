abc1f88ea097c6134f85bdbd9dc2bd6c4ccdc351
Brandon Simpkins <brandonsimpkins@gmail.com>
2018-07-06 04:05:41 +0000

    Updated serializers to be Hyperlinked
    Upodated the serializer base classes to be HyperlinkedModelSerializer
    instances. Added identity fields for the items and unit-of-issue data
    values to have specific URLs thatc an be linked to.

--- src/fd_supply_services/item_service/serializers.py
+++ src/fd_supply_services/item_service/serializers.py
@@ -1,16 +1,27 @@
 from rest_framework import serializers
 from item_service.models import Item, UnitOfIssue
 
-class UnitOfIssueSerializer(serializers.ModelSerializer):
+class UnitOfIssueSerializer(serializers.HyperlinkedModelSerializer):
     """
     """
+    url = serializers.HyperlinkedIdentityField(
+            read_only = True,
+            view_name='unitofissue-detail',
+            format='html',
+            lookup_field='code')
+
     class Meta:
         model = UnitOfIssue
-        fields = ('code', 'description')
+        fields = '__all__'
 
 class ItemSerializer(serializers.ModelSerializer):
     """
     """
+
+    url = serializers.HyperlinkedIdentityField(
+            view_name='item-detail',
+            format='html',
+            lookup_field='nsn')
 
     class Meta:
         model = Item
