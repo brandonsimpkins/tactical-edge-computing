6d958d47a50a079296ceda4587539662a8d7de81
Brandon Simpkins <brandonsimpkins@gmail.com>
2018-07-06 03:33:17 +0000

    Updated django db settings to use postgres locally.
    Specified basic username / passowrd settings to align with the docker
    compose file. Need to adjust these settings so that they are set
    securely.

--- src/fd_supply_services/fd_supply_services/settings.py
+++ src/fd_supply_services/fd_supply_services/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'fd_supply_services.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'changeme',
+        'HOST': 'supply-db',
+        'PORT': 5432,
     }
 }
 
