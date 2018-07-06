a4b2f7320e25863507785a3c6b4a6edf752ccace
Brandon Simpkins <brandonsimpkins@gmail.com>
2018-07-06 03:50:49 +0000

    Fixed detail views and added a root api view
    Updated the url patterns to (1) have proper regexes (2) specify the
    name of the associated primary key field in the model class (3) assigned
    names to the views so they they could be referenced elsewhere.
    
    Updated the UnitOfIssueList and ItemList views to perform lookusp
    againstthe model primary key.
    
    Added a default view for the root of the API that list the URLS that the
    item-service app can respond to.

--- src/fd_supply_services/item_service/urls.py
+++ src/fd_supply_services/item_service/urls.py
@@ -2,9 +2,18 @@ from django.conf.urls import url
 from item_service import views
 
 urlpatterns = [
-    url(r'^items/$', views.ItemList.as_view()),
-    url(r'^items/(?P<pk>[0-9]+)/$', views.ItemDetail.as_view()),
-    url(r'^unit-of-issue/$', views.UnitOfIssueList.as_view()),
-    url(r'^unit-of-issue/(?P<pk>[0-9]+)/$', views.UnitOfIssueDetail.as_view()),
+    url(r'^$', views.api_root),
+    url(r'^item/$',
+        views.ItemList.as_view(),
+        name="item-list"),
+    url(r'^item/(?P<nsn>[0-9A-Za-z]+)/$',
+        views.ItemDetail.as_view(),
+        name="item-detail"),
+    url(r'^unit-of-issue/$',
+        views.UnitOfIssueList.as_view(),
+        name="unitofissue-list"),
+    url(r'^unit-of-issue/(?P<code>[A-Z]+)/$',
+        views.UnitOfIssueDetail.as_view(),
+        name="unitofissue-detail"),
 
 ]
--- src/fd_supply_services/item_service/views.py
+++ src/fd_supply_services/item_service/views.py
@@ -2,6 +2,18 @@
 from item_service.models import Item, UnitOfIssue
 from item_service.serializers import ItemSerializer, UnitOfIssueSerializer
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'items': reverse('item-list', request=request, format=format),
+        'units of issue': reverse('unitofissue-list', request=request, format=format)
+    })
 
 
 class UnitOfIssueList(generics.ListCreateAPIView):
@@ -11,6 +23,7 @@ class UnitOfIssueList(generics.ListCreateAPIView):
 class UnitOfIssueDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UnitOfIssue.objects.all()
     serializer_class = UnitOfIssueSerializer
+    lookup_field = 'code'
 
 class ItemList(generics.ListCreateAPIView):
     queryset = Item.objects.all()
@@ -19,3 +32,4 @@ class ItemList(generics.ListCreateAPIView):
 class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    lookup_field = 'nsn'
