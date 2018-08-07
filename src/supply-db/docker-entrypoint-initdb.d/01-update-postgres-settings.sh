3c7c2f002f7b59c305f147e895ad8f29c38a8dc0
Brandon Simpkins <brandonsimpkins@gmail.com>
2018-08-07 20:23:30 +0000

    Added default HBA rep entry (NOT SECURE!)
    To simplify the demo process, I updated the init sctipts to add a HBA
    replciation entry that allows and replication client to connect in as
    long as they have the right password. This makes sure I don't have to
    tink with the CIDR range before the demo on a snowball with a static IP
    address.

--- src/supply-db/docker-entrypoint-initdb.d/01-update-postgres-settings.sh
+++ src/supply-db/docker-entrypoint-initdb.d/01-update-postgres-settings.sh
@@ -14,3 +14,13 @@ cat  $PGDATA/postgresql.conf | grep 'max_replication_slots'
 cat  $PGDATA/postgresql.conf | grep 'max_wal_senders'
 cat  $PGDATA/postgresql.conf | grep 'wal_sender_timeout'
 cat  $PGDATA/postgresql.conf | grep 'idle_in_transaction_session_timeout'
+
+REP_ENTRY="host replication all all md5"
+
+if [ $(grep "$REP_ENTRY" $PGDATA/pg_hba.conf) ]; then
+  echo "Replication entry already exists in pg_hba.conf"
+else
+  echo "$REP_ENTRY" >> $PGDATA/pg_hba.conf
+  echo "Replication entry added to pg_hba.conf"
+fi
+
