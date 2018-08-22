15f9d7fb286ac69f15cc2dad72c1ae7d9d2d7681
Brandon Simpkins <brandonsimpkins@gmail.com>
2018-08-22 17:44:19 +0000

    Added item data, updated item model
    Added item reference data in CSV format, and the commands to load it
    into the database.
    
    The item model required a chnage to the precision of the price, loading
    the bata balked on a price (10534.24) that was over the 6 digit
    precision. I bumped it up to an arbitrary number, no real need to
    optimize. Added the migration.

--- src/fd_supply_services/item_service/models.py
+++ src/fd_supply_services/item_service/models.py
@@ -36,7 +36,7 @@ class Item(models.Model):
     category = models.CharField(max_length=100, blank=True, default='')
     common_name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
     ui = models.ForeignKey(UnitOfIssue)
     aac = models.CharField("acquisition advice code", max_length=1)
     # created = models.DateTimeField(auto_now_add=True)
